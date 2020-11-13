49be0dd7f534a60542399bb3fb4a48e542d0cbe2
Arm-And-Hammer <73447422+Arm-And-Hammer@users.noreply.github.com>
2020-11-13 16:59:44 -0500

    Inital commit and finish

---
+++ js/main.js
@@ -0,0 +1,45 @@
+/*----- constants -----*/
+
+/*----- app's state (variables) -----*/
+let total;
+
+/*----- cached element references -----*/
+let addButton = document.querySelector("#add");
+let subtractButton = document.querySelector("#minus");
+let num = document.querySelector("#numbers");
+let totalNumber = document.querySelector("span");
+/*----- event listeners -----*/
+addButton.addEventListener("click", handleAdd);
+subtractButton.addEventListener("click", handleSubtract);
+
+/*----- functions -----*/
+function init(){
+    total = 0;
+    render();
+}
+
+function render(){
+    if (total >= 0) {
+        totalNumber.innerHTML = `
+        <span class="green">+${total}</span>
+        `
+      } else {
+        totalNumber.innerHTML = `
+        <span class="red">${total}</span>
+        `
+      }
+}
+
+function handleAdd() {
+    let numbers = parseInt(num.value);
+    total += ( + num.value)
+    render();
+  }
+function handleSubtract(){
+    let numbers = parseInt(num.value);
+    total += ( - num.value)
+    render();
+}
+init();
+
+
